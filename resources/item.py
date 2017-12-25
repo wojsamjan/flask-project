3ea25571855e4574ee7cb7789a24e9b47572baed
Wojciech Januszek <wojsamjan@gmail.com>
2017-12-25 17:08:49 +0100

    add branch index to request parser, using branch finding mechanism

--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
+from models.branch import BranchModel
 
 
 class Item(Resource):
@@ -35,16 +36,23 @@ class Item(Resource):
         required=True,
         help="Every item needs a model!"
     )
+    parser.add_argument('branch_id',
+        type=int,
+        required=True,
+        help="Every item needs a branch_id!"
+    )
 
-    def get(self, name):
-        item = ItemModel.find_by_name(name)
+    def get(self, branch_name, name):
+        branch = BranchModel.find_by_name(branch_name)
+        item = ItemModel.find_by_name_in_branch(branch.id, name)
         if item:
-            return item.json()
+            return item.short_json()
         return {'message': 'Item not found.'}, 404
 
     @jwt_required()
-    def post(self, name):
-        if ItemModel.find_by_name(name):
+    def post(self, branch_name, name):
+        branch = BranchModel.find_by_name(branch_name)
+        if ItemModel.find_by_name_in_branch(branch.id, name):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
 
         data = Item.parser.parse_args()
@@ -59,18 +67,20 @@ class Item(Resource):
         return item.json(), 201
 
     @jwt_required()
-    def delete(self, name):
-        item = ItemModel.find_by_name(name)
+    def delete(self, branch_name, name):
+        branch = BranchModel.find_by_name(branch_name)
+        item = ItemModel.find_by_name_in_branch(branch.id, name)
         if item:
             item.delete_from_db()
 
         return {'message': 'Item deleted.'}
 
     @jwt_required()
-    def put(self, name):
+    def put(self, branch_name, name):
+        branch = BranchModel.find_by_name(branch_name)
         data = Item.parser.parse_args()
 
-        item = ItemModel.find_by_name(name)
+        item = ItemModel.find_by_name_in_branch(branch.id, name)
 
         if item is None:
             item = ItemModel(name, **data)  # data['price'], data['store_id']
@@ -82,14 +92,17 @@ class Item(Resource):
             item.model = data['model']
             # item.store_id = data['store_id']
 
+            item.branch_id = data['branch_id']
+
         item.save_to_db()
 
         return item.json()
 
 
 class ItemReserve(Resource):
-    def put(self, name):
-        item = ItemModel.find_by_name(name)
+    def put(self, branch_name, name):
+        branch = BranchModel.find_by_name(branch_name)
+        item = ItemModel.find_by_name_in_branch(branch.id, name)
 
         if item is None:
             return {'message': 'Item does not exist.'}
@@ -101,14 +114,20 @@ class ItemReserve(Resource):
 
         item.save_to_db()
 
-        return item.json()
+        return item.short_json()
 
 
 class ItemList(Resource):
-    def get(self, param="", value_p=""):
+    def get(self, branch_name="", param="", value_p=""):
+        branch = BranchModel.find_by_name(branch_name)
+        if not branch:
+            return {'message': 'Branch not found.'}, 404
         # if param == "item-type" and (value_p == "ski" or value_p == "snowboard" or value_p == "surfing-board" or value_p == "pedalo" or value_p == "bike" or value_p == "rollerblades" or value_p == "longboard" or value_p == "tent" or value_p == "sleeping-bag" or value_p == "gps" or value_p == "caravan" or value_p == "cool-box" or value_p == "rucksack"):
         if param == "item-type" and ItemModel.is_item_type(value_p):
-            return {'items': [item.json() for item in ItemModel.query.filter_by(item_type=value_p)]}
+            return {'items': [item.short_json() for item in ItemModel.query.filter_by(item_type=value_p, branch_id=branch.id)]}
+        elif not param:
+            return {'items': [item.short_json() for item in ItemModel.query.filter_by(branch_id=branch.id)]}
+            # return {'items': [item.json() for item in ItemModel.query.all()]}  # list comprehension
         else:
-            return {'items': [item.json() for item in ItemModel.query.all()]}  # list comprehension
+            return {'message': 'Wrong parameters of request!'}
             # return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}  # lambda, mapping func() to elements
