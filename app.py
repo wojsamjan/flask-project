aafe56c75027d2010fc012e308a3dc5ee64b3362
Wojciech Januszek <wojsamjan@gmail.com>
2017-12-26 15:07:19 +0100

    update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from resources.item import Item, ItemList, ItemReserve
 # from resources.store import Store, StoreList
 from resources.car import Car, CarList, CarReserve
 from resources.branch import Branch, BranchList
+from resources.position import Position, PositionList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -23,20 +24,25 @@ def create_tables():
 jwt = JWT(app, authenticate, identity)  # /auth
 
 # api.add_resource(Store, '/store/<string:name>')
-api.add_resource(Item, '/item/<string:name>')  # http://127.0.0.1:5000/item/item_name
-api.add_resource(ItemList, '/items',
-                 '/items/<string:param>/<string:value_p>')
-api.add_resource(ItemReserve, '/item/reserve/<string:name>')
 # api.add_resource(StoreList, '/stores')
 
-api.add_resource(Car, '/car/<string:name>')
-api.add_resource(CarList, '/cars',
-                 '/cars/<string:param>/<string:value_p>')
-api.add_resource(CarReserve, '/car/reserve/<string:name>')
+api.add_resource(Item, '/<string:branch_name>/item/<string:name>')  # http://127.0.0.1:5000/branch_namr/item/item_name
+api.add_resource(ItemList, '/<string:branch_name>/items',
+                 '/<string:branch_name>/items/<string:param>/<string:value_p>')
+api.add_resource(ItemReserve, '/<string:branch_name>/item/reserve/<string:name>')
+
+api.add_resource(Car, '/<string:branch_name>/car/<string:name>')
+api.add_resource(CarList, '/<string:branch_name>/cars',
+                 '/<string:branch_name>/cars/<string:param>/<string:value_p>')
+api.add_resource(CarReserve, '/<string:branch_name>/car/reserve/<string:name>')
 
 api.add_resource(Branch, '/branch/<string:name>')
 api.add_resource(BranchList, '/branches',
                  '/branches/<string:country>')
+
+api.add_resource(Position, '/position/<string:name>')
+api.add_resource(PositionList, '/positions',
+                 '/<string:branch_name>/positions')
 
 api.add_resource(UserRegister, '/register')
 
