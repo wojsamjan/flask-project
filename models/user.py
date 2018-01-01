57ae058c59d61e38294f9a5a04eee5987118a113
Wojciech Januszek <wojsamjan@gmail.com>
2018-01-01 17:01:44 +0100

    add deleting user from database

--- models/user.py
+++ models/user.py
@@ -50,6 +50,10 @@ class UserModel(db.Model):  # db.Model -> tells SQLAlchemy about relation
         db.session.add(self)
         db.session.commit()
 
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
     @classmethod
     def find_by_username(cls, username):
         return cls.query.filter_by(username=username).first()
