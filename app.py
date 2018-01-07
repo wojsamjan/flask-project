c719635c05e0686b2f6382832c02f3ae96cfcb45
Wojciech Januszek <wojsamjan@gmail.com>
2018-01-08 00:36:28 +0100

    fixed not importing db when running the app

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ app.secret_key = 'stan'
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
