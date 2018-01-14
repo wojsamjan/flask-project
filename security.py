7bd1d2d511fa6807812bc0207d724b6bf3fc0297
Wojciech Januszek <wojsamjan@gmail.com>
2018-01-14 21:55:22 +0100

    update security.py

--- security.py
+++ security.py
@@ -1,15 +1,19 @@
+# from werkzeug.security import safe_str_cmp
 from flask import g
 from models.user import UserModel
 from models.customer import CustomerModel
+# from models.position import PositionModel
 
 
 def authenticate(username, password):
     user = UserModel.find_by_username(username)
     customer = CustomerModel.find_by_username(username)
-    # print('authXXX')
+    print('authXXX')
     if user and user.verify_password(password):
+        print('USER-auth')
         return user
     elif customer and customer.verify_password(password):
+        print('CUSTOMER-auth')
         return customer
 
 
