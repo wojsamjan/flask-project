1447cfcd55af4eee3b7a66402c9b92f0521eb59d
Wojciech Januszek <wojsamjan@gmail.com>
2018-01-17 10:34:07 +0100

    update shortened json

--- models/car.py
+++ models/car.py
@@ -62,9 +62,9 @@ class CarModel(db.Model):
 
     def short_json(self):
         return {
-                'price': self.price, 'available': self.available, 'year': self.year, 'car_type': self.car_type,
-                'vendor': self.vendor, 'model': self.model, 'seats': self.seats, 'transmission': self.transmission,
-                'drive': self.drive, 'fuel': self.fuel
+                'name': self.name, 'price': self.price, 'available': self.available, 'year': self.year,
+                'car_type': self.car_type, 'vendor': self.vendor, 'model': self.model, 'seats': self.seats,
+                'transmission': self.transmission, 'drive': self.drive, 'fuel': self.fuel
                 }
 
     @classmethod
