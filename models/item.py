75293a7d0038001dbee18d4a8d432a394d266668
Wojciech Januszek <wojsamjan@gmail.com>
2018-01-17 10:34:29 +0100

    update shortened json

--- models/item.py
+++ models/item.py
@@ -37,8 +37,8 @@ class ItemModel(db.Model):
                 'item_type': self.item_type, 'vendor': self.vendor, 'model': self.model, 'branch_id': self.branch_id}
 
     def short_json(self):
-        return {'price': self.price, 'available': self.available, 'year': self.year, 'item_type': self.item_type,
-                'vendor': self.vendor, 'model': self.model}
+        return {'name': self.name, 'price': self.price, 'available': self.available, 'year': self.year,
+                'item_type': self.item_type, 'vendor': self.vendor, 'model': self.model}
 
     @classmethod
     def find_by_name(cls, name):
