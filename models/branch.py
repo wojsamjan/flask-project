287890afcba77b3b6c4e8da28bb7404d0b1b63d5
Wojciech Januszek <wojsamjan@gmail.com>
2018-01-19 16:09:07 +0100

    add finding branch by id in models/branch.py

--- models/branch.py
+++ models/branch.py
@@ -52,6 +52,10 @@ class BranchModel(db.Model):
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
 
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
+
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
