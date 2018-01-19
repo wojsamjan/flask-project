a47d1e5e6a367cf8197f248d4717b94a7a7735ab
Wojciech Januszek <wojsamjan@gmail.com>
2018-01-19 18:37:55 +0100

    update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ api.add_resource(ItemListAdmin, '/items',
 api.add_resource(ItemReserve, '/<string:branch_name>/item/reserve/<string:name>')
 api.add_resource(ItemCancelReservation, '/<string:branch_name>/item/cancel-reservation/<string:name>')
 api.add_resource(ItemReservedByList, '/item-reserved-by-list')
-api.add_resource(ItemReservedListForAdmin, '/item-reserved-list')
+api.add_resource(ItemReservedListForAdmin, '/item-reserved-list-for-admin/<string:username>')
 
 api.add_resource(Car, '/<string:branch_name>/car/<string:name>')
 api.add_resource(CarList, '/<string:branch_name>/cars',
@@ -54,7 +54,7 @@ api.add_resource(CarListAdmin, '/cars',
 api.add_resource(CarReserve, '/<string:branch_name>/car/reserve/<string:name>')
 api.add_resource(CarCancelReservation, '/<string:branch_name>/car/cancel-reservation/<string:name>')
 api.add_resource(CarReservedByList, '/car-reserved-by-list')
-api.add_resource(CarReservedListForAdmin, '/car-reserved-list')
+api.add_resource(CarReservedListForAdmin, '/car-reserved-list-for-admin/<string:username>')
 
 api.add_resource(Branch, '/branch/<string:name>')
 api.add_resource(BranchList, '/branches',
