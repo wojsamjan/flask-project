bc183caad608ec8a75456581be2f11fe7bba6c7a
Wojciech Januszek <wojsamjan@gmail.com>
2018-01-20 15:22:48 +0100

    add country_city_validator

--- helpers/validators.py
+++ helpers/validators.py
@@ -2,3 +2,11 @@ from string import ascii_letters as letters
 from string import ascii_lowercase as lowercase
 from string import ascii_uppercase as uppercase
 from string import digits
+
+
+def country_city_validator(country):
+    country_city_regex = letters + ' ' + '-'
+    for sign in country:
+        if sign not in country_city_regex:
+            return False
+    return True
