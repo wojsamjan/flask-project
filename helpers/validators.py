b9e00c6177c9e6f3dc54153d4b24e4a80f1447b7
Wojciech Januszek <wojsamjan@gmail.com>
2018-01-20 15:33:39 +0100

    add street_validator

--- helpers/validators.py
+++ helpers/validators.py
@@ -11,9 +11,18 @@ def country_city_validator(country):
             return False
     return True
 
+
 def postal_code_validator(postal_code):
     postal_regex = digits + ' ' + '-'
     for sign in postal_code:
         if sign not in postal_regex:
+            return False
+    return True
+
+
+def street_validator(street):
+    street_regex = letters + digits + ' ' + '-'
+    for sign in street:
+        if sign not in street_regex:
             return False
     return True
