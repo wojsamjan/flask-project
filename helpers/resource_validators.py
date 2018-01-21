5af239d3255964b328ebf860e57edf934e1fff7e
Wojciech Januszek <wojsamjan@gmail.com>
2018-01-21 14:57:45 +0100

    add customer delete and user validators

--- helpers/resource_validators.py
+++ helpers/resource_validators.py
@@ -1,6 +1,9 @@
 import helpers.validators as hv
 
 
+# position password
+
+
 def branch_validator(country, city, postal_code, street, email, phone):
     args = [country, city, postal_code, street, email, phone]
     validators = [hv.country_validator, hv.city_validator, hv.postal_code_validator, hv.street_validator,
@@ -17,12 +20,11 @@ def branch_validator(country, city, postal_code, street, email, phone):
     return {'error results': error_results}
 
 
-# def customer_validator(username, password, first_name, last_name, email, phone):
-def customer_validator(username, first_name, last_name, email, phone):
-    # args = [username, password, first_name, last_name, email, phone]
+# password
+def customer_register_validator(username, first_name, last_name, email, phone):
+    # password
     args = [username, first_name, last_name, email, phone]
-    # validators = [hv.username_validator, hv.password_validator, hv.first_name_validator, hv.last_name_validator,
-    #               hv.email_validator, hv.phone_validator]
+    # password
     validators = [hv.username_validator, hv.first_name_validator, hv.last_name_validator, hv.email_validator,
                   hv.phone_validator]
 
@@ -35,6 +37,36 @@ def customer_validator(username, first_name, last_name, email, phone):
     return {'error results': error_results}
 
 
-# username password first_name last_name country city postal_code street email phone branch_id position_id salary
-def user_validator():
-    pass
+# customer change password
+
+
+# password
+def customer_delete_validator(username):
+    args = [username]
+    validators = [hv.username_validator]
+
+    if len(args) != len(validators):
+        return {'customer validator': 'Incorrect number of passed arguments.'}
+
+    validator_results = [validator(arg) for validator in validators for arg in args]
+    error_results = [result for result in validator_results if len(result['validator message']) > 2]
+
+    return {'error results': error_results}
+
+
+# password
+def user_validator(username, first_name, last_name, country, city, postal_code, street, email, phone, branch_id,
+                   position_id, salary):
+    args = [username, first_name, last_name, country, city, postal_code, street, email, phone, branch_id,
+            position_id, salary]
+    validators = [hv.username_validator, hv.first_name_validator, hv.last_name_validator, hv.country_validator,
+                  hv.city_validator, hv.postal_code_validator, hv.street_validator, hv.email_validator,
+                  hv.phone_validator, hv.branch_id_validator, hv.position_id_validator, hv.salary_validator]
+
+    if len(args) != len(validators):
+        return {'branch validator': 'Incorrect number of passed arguments.'}
+
+    validator_results = [validator(arg) for validator in validators for arg in args]
+    error_results = [result for result in validator_results if len(result['validator message']) > 2]
+
+    return {'error results': error_results}
