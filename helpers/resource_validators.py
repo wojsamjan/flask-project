d90d6c42575bdeec923b2edcac33dae017114191
Wojciech Januszek <wojsamjan@gmail.com>
2018-01-21 19:31:53 +0100

    add common validator for the rest of validators

--- helpers/resource_validators.py
+++ helpers/resource_validators.py
@@ -1,17 +1,23 @@
 import helpers.validators as hv
 
 
+def validator(args, validators, source):
+    if len(args) != len(validators):
+        return {source: 'Incorrect number of passed arguments.'}
+
+    validator_results = [validator(arg) for validator in validators for arg in args]
+    # print(list(filter(lambda x: x != 13, my_list)))
+    # [y for y in a if y not in b]
+    error_results = [result for result in validator_results if len(result['validation message']) > 2]
+
+    return {'error validation': error_results}
+
+
 def position_validator(password):
     args = [password]
     validators = [hv.password_validator]
 
-    if len(args) != len(validators):
-        return {'customer validator': 'Incorrect number of passed arguments.'}
-
-    validator_results = [validator(arg) for validator in validators for arg in args]
-    error_results = [result for result in validator_results if len(result['validator message']) > 2]
-
-    return {'error results': error_results}
+    return validator(args, validators, source='position validator')
 
 
 def branch_validator(country, city, postal_code, street, email, phone):
@@ -19,15 +25,7 @@ def branch_validator(country, city, postal_code, street, email, phone):
     validators = [hv.country_validator, hv.city_validator, hv.postal_code_validator, hv.street_validator,
                   hv.email_validator, hv.phone_validator]
 
-    if len(args) != len(validators):
-        return {'branch validator': 'Incorrect number of passed arguments.'}
-
-    validator_results = [validator(arg) for validator in validators for arg in args]
-    # print(list(filter(lambda x: x != 13, my_list)))
-    # [y for y in a if y not in b]
-    error_results = [result for result in validator_results if len(result['validator message']) > 2]
-
-    return {'error results': error_results}
+    return validator(args, validators, source='branch validator')
 
 
 def customer_register_validator(username, password, first_name, last_name, email, phone):
@@ -35,39 +33,21 @@ def customer_register_validator(username, password, first_name, last_name, email
     validators = [hv.username_validator, hv.password_validator, hv.first_name_validator, hv.last_name_validator,
                   hv.email_validator, hv.phone_validator]
 
-    if len(args) != len(validators):
-        return {'customer validator': 'Incorrect number of passed arguments.'}
-
-    validator_results = [validator(arg) for validator in validators for arg in args]
-    error_results = [result for result in validator_results if len(result['validator message']) > 2]
-
-    return {'error results': error_results}
+    return validator(args, validators, source='customer-register validator')
 
 
 def change_password_validator(old_password, new_password):
     args = [old_password, new_password]
     validators = [hv.password_validator, hv.password_validator]
 
-    if len(args) != len(validators):
-        return {'customer validator': 'Incorrect number of passed arguments.'}
-
-    validator_results = [validator(arg) for validator in validators for arg in args]
-    error_results = [result for result in validator_results if len(result['validator message']) > 2]
-
-    return {'error results': error_results}
+    return validator(args, validators, source='change-password validator')
 
 
 def delete_validator(username, password):
     args = [username, password]
     validators = [hv.username_validator, hv.password_validator]
 
-    if len(args) != len(validators):
-        return {'customer validator': 'Incorrect number of passed arguments.'}
-
-    validator_results = [validator(arg) for validator in validators for arg in args]
-    error_results = [result for result in validator_results if len(result['validator message']) > 2]
-
-    return {'error results': error_results}
+    return validator(args, validators, source='delete validator')
 
 
 def user_register_validator(username, password, first_name, last_name, country, city, postal_code, street, email,
@@ -79,13 +59,7 @@ def user_register_validator(username, password, first_name, last_name, country, 
                   hv.email_validator, hv.phone_validator, hv.branch_id_validator, hv.position_id_validator,
                   hv.salary_validator]
 
-    if len(args) != len(validators):
-        return {'branch validator': 'Incorrect number of passed arguments.'}
-
-    validator_results = [validator(arg) for validator in validators for arg in args]
-    error_results = [result for result in validator_results if len(result['validator message']) > 2]
-
-    return {'error results': error_results}
+    return validator(args, validators, source='user-register validator')
 
 
 def item_validator(price, year, item_type, vendor, model, branch_id):
@@ -93,13 +67,7 @@ def item_validator(price, year, item_type, vendor, model, branch_id):
     validators = [hv.price_validator, hv.year_validator, hv.item_type_validator, hv.vendor_validator,
                   hv.model_validator, hv.branch_id_validator]
 
-    if len(args) != len(validators):
-        return {'branch validator': 'Incorrect number of passed arguments.'}
-
-    validator_results = [validator(arg) for validator in validators for arg in args]
-    error_results = [result for result in validator_results if len(result['validator message']) > 2]
-
-    return {'error results': error_results}
+    return validator(args, validators, source='item validator')
 
 
 def car_validator(price, year, car_type, vendor, model, colour, seats, transmission, drive, fuel, engine_power,
@@ -109,10 +77,4 @@ def car_validator(price, year, car_type, vendor, model, colour, seats, transmiss
                   hv.model_validator, hv.colour_validator, hv.seats_validator, hv.transmission_validator,
                   hv.drive_validator, hv.fuel_validator, hv.engine_power_validator, hv.branch_id_validator]
 
-    if len(args) != len(validators):
-        return {'branch validator': 'Incorrect number of passed arguments.'}
-
-    validator_results = [validator(arg) for validator in validators for arg in args]
-    error_results = [result for result in validator_results if len(result['validator message']) > 2]
-
-    return {'error results': error_results}
+    return validator(args, validators, source='car validator')
