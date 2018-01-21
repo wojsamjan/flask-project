2c989a6bef7680b47d15580becd210ce22445a74
Wojciech Januszek <wojsamjan@gmail.com>
2018-01-21 14:41:04 +0100

    add customer validator

--- helpers/resource_validators.py
+++ helpers/resource_validators.py
@@ -15,3 +15,26 @@ def branch_validator(country, city, postal_code, street, email, phone):
     error_results = [result for result in validator_results if len(result['validator message']) > 2]
 
     return {'error results': error_results}
+
+
+# def customer_validator(username, password, first_name, last_name, email, phone):
+def customer_validator(username, first_name, last_name, email, phone):
+    # args = [username, password, first_name, last_name, email, phone]
+    args = [username, first_name, last_name, email, phone]
+    # validators = [hv.username_validator, hv.password_validator, hv.first_name_validator, hv.last_name_validator,
+    #               hv.email_validator, hv.phone_validator]
+    validators = [hv.username_validator, hv.first_name_validator, hv.last_name_validator, hv.email_validator,
+                  hv.phone_validator]
+
+    if len(args) != len(validators):
+        return {'customer validator': 'Incorrect number of passed arguments.'}
+
+    validator_results = [validator(arg) for validator in validators for arg in args]
+    error_results = [result for result in validator_results if len(result['validator message']) > 2]
+
+    return {'error results': error_results}
+
+
+# username password first_name last_name country city postal_code street email phone branch_id position_id salary
+def user_validator():
+    pass
