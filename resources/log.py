19ecf59c18368f9d9b7d053b2efeedf92b584401
Wojciech Januszek <wojsamjan@gmail.com>
2018-01-22 22:34:21 +0100

    add Log resource in resources/log.py

---
+++ resources/log.py
@@ -0,0 +1,32 @@
+from flask_restful import Resource
+from flask_jwt import jwt_required
+# from flask import g
+import helpers.authorizators as auth
+from models.log import LogModel
+
+
+class Log(Resource):
+    @jwt_required()
+    def get(self, _id):
+        log = LogModel.find_by_id(_id)
+        is_admin = auth.is_admin()
+
+        if log:
+            if not is_admin:
+                return {'message': 'You are not privileged to continue!'}, 400
+            return log.json()
+
+        return {'message': 'Log not found.'}, 404
+
+    @jwt_required()
+    def delete(self, _id):
+        log = LogModel.find_by_id(_id)
+        is_admin = auth.is_admin()
+
+        if log:
+            if not is_admin:
+                return {'message': 'You are not privileged to continue!'}, 400
+            log.delete_from_db()
+            return {'message': 'Item deleted.'}
+
+        return {'message': 'Log not found.'}, 404
