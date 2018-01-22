402f5556b414adfa53a61483e00133b77b8b38ce
Wojciech Januszek <wojsamjan@gmail.com>
2018-01-22 22:19:28 +0100

    add authorization helpers

---
+++ helpers/authorizators.py
@@ -0,0 +1,41 @@
+from flask import g
+from models.position import PositionModel
+
+
+# resources/item.py
+admin = 'admin'
+manager = 'manager'
+
+
+def is_employee():
+    try:
+        if g.user:
+            return True
+    except:
+        return False
+
+
+def is_manager():
+    is_user = is_employee()
+    if not is_user:
+        return False
+
+    user = g.user
+    user_position = PositionModel.find_by_id(user.position_id)
+
+    if user_position.name != manager:
+        return False
+    return True
+
+
+def is_admin():
+    is_user = is_employee()
+    if not is_user:
+        return False
+
+    user = g.user
+    user_position = PositionModel.find_by_id(user.position_id)
+
+    if user_position.name != admin:
+        return False
+    return True
